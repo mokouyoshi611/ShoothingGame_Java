fce8743e398848e2718c79714995f04c38e260c6
mok <jojy0628@gmail.com>
2024-06-18 10:05:23 +0900

    11.2

--- Shoothing/src/GameFrame.java
+++ Shoothing/src/GameFrame.java
@@ -5,11 +5,14 @@ public class GameFrame extends MyFrame{
 		GameWorld.player=new Player(100,300,0,0);
 		addKeyListener(GameWorld.player);
 		GameWorld.playerBullets=new Vector<PlayerBullet>();
+		GameWorld.enemies=new Vector<Enemy>();
+		GameWorld.enemies.add(new EnemyBase(100,50,1,0));
 		while(true) {
 			clear();
 		GameWorld.player.draw(this);
 		GameWorld.player.move();
 		movePlayerBullets();
+        moveEnemies();
 		sleep(0.03);
 		
 		
@@ -20,10 +23,9 @@ public class GameFrame extends MyFrame{
     	int i=0;
     	while(i<GameWorld.playerBullets.size()) {
     		PlayerBullet b=GameWorld.playerBullets.get(i);
-    		if(true) {
     		b.draw(this);
     		b.move();
-    		}
+    		
 
     		
     		
@@ -36,6 +38,13 @@ public class GameFrame extends MyFrame{
     			
     	}
     	
+    }
+    public void moveEnemies() {
+		for(int i=0 ; i<GameWorld.enemies.size(); i++) {
+			Enemy e=GameWorld.enemies.get(i);
+			e.draw(this);
+			e.move();
+		}
     }
 
 }
